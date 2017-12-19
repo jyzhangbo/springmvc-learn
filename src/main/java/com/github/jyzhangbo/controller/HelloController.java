ac059cdb0bb21f44b866bcb8e362227d4e460f1d
zhangboad <zhangboad@enn.cn>
2017-12-19 16:30:44 +0800

    添加security

--- src/main/java/com/github/jyzhangbo/controller/HelloController.java
+++ src/main/java/com/github/jyzhangbo/controller/HelloController.java
@@ -1,20 +1,44 @@
 package com.github.jyzhangbo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author zhangbo
  *
  */
 @Controller
-@RequestMapping(value = "/v2")
 public class HelloController {
 
+  @RequestMapping(value = "/login", method = RequestMethod.GET)
+  public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+      @RequestParam(value = "logout", required = false) String logout) {
+    ModelAndView model = new ModelAndView();
+    if (error != null) {
+      model.addObject("error", "Invalid username and password!");
+    }
+
+    if (logout != null) {
+      model.addObject("msg", "You've been logged out successfully.");
+    }
+    model.setViewName("login");
+    return model;
+  }
+
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
-  public String hello() {
+  public String hello(ModelMap model) {
+    model.addAttribute("name", "zhangbo");
     return "hello";
+  }
+
+  @RequestMapping(value = "/admin", method = RequestMethod.POST)
+  public String admin(ModelMap model) {
+    model.addAttribute("name", "admin");
+    return "admin";
   }
 
 }
--- src/main/webapp/WEB-INF/pages/hello.jsp
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,3 +1,4 @@
+<%@ page session="false"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -7,6 +8,6 @@
 <title>hello</title>
 </head>
 <body>
-	hello
+	hello ${name}
 </body>
 </html>
